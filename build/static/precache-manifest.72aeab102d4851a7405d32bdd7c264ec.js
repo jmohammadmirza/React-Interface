22b4512e405f61c614c1202dd59e07dab7909137
Mohammad Javed <mdaslameme@gmail.com>
2019-10-07 11:26:04 +0400

    Create precache-manifest.72aeab102d4851a7405d32bdd7c264ec.js

---
+++ build/static/precache-manifest.72aeab102d4851a7405d32bdd7c264ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ea538510f4a8cd127ea6",
+    "url": "/static/js/main.f35ad56c.chunk.js"
+  },
+  {
+    "revision": "f9413e33a77e3e28e7f4",
+    "url": "/static/js/2.96c783f3.chunk.js"
+  },
+  {
+    "revision": "ea538510f4a8cd127ea6",
+    "url": "/static/css/main.db2f0f55.chunk.css"
+  },
+  {
+    "revision": "f9413e33a77e3e28e7f4",
+    "url": "/static/css/2.56992e5a.chunk.css"
+  },
+  {
+    "revision": "856245f03ac8390a13194122e74ac790",
+    "url": "/index.html"
+  }
+];
